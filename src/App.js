92c2dc282a310d08041a000294d4e75ab1c3a481
Nodirbek2211 <72076091+Nodirbek2211@users.noreply.github.com>
2021-04-27 10:43:01 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import Timer from "./Components/Timer";
+import Secundamer from "./Components/Secundamer";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    const [status, setStatus] = useState(false)
+    const [modal, setModal] = useState(false)
+
+    function closeModal() {
+        setStatus(false)
+    }
+
+    function isSecundomer(){
+        setStatus(true)
+        setModal(true)
+    }
+
+    function isTimer(){
+        setStatus(true)
+        setModal(false)
+    }
+
+    return <div className={'container'}>
+        <div className="row justify-content-center mt-5">
+            <div className="col-md-6">
+                {
+                    !status ? <div className={'container'}>
+                        <div className="row m-5 justify-content-center">
+                            <div className="col-md-12">
+                                <h3>Click here to <button className={'btn btn-success'} onClick={isSecundomer}>Secondmer</button></h3>
+                                <h3>Click here to <button className={'btn btn-success'} onClick={isTimer}>Timer</button></h3>
+
+
+                            </div>
+                        </div>
+                    </div> : (modal ? <Secundamer closeModal={closeModal}/> : <Timer closeModal={closeModal}/>)
+                }
+
+            </div>
+        </div>
     </div>
-  );
+
 }
 
-export default App;
+export default App
